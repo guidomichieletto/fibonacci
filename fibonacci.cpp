34adbfcac68078f3bb4ff15e615b0a790ea3cbfc
Guido <36931325+guidomichieletto@users.noreply.github.com>
2023-05-27 10:58:53 +0200

    Frist commit

---
+++ fibonacci.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int pos){
+	if(pos <= 1) return pos;
+	return fibonacci(pos - 1) + fibonacci(pos - 2);
+}
+
+int main(){
+	int pos = 0;
+	cin >> pos;
+	cout << fibonacci(pos);
+}
